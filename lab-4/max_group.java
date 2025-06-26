2373bd07758ff4dce282e1bb5d70ce30db7e3cb1
mannpadhiar <mannpadhiar22@gmail.com>
2025-06-26 09:19:59 +0530

    added lab-4

---
+++ lab-4/max_group.java
@@ -0,0 +1,36 @@
+class MaxGroup{
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,1,1,1},
+            {1,1,1,0},
+            {1,1,1,1},
+            {1,0,1,1},
+        };
+
+        DFSTree dFSTree = new DFSTree();
+        boolean[] visited = new boolean[arr.length];
+        int count = 0;
+
+        for(int i=0;i<arr.length;i++){
+            if(!visited[i]){
+                dFSTree.dfs(arr, visited, i);
+                count++;
+            }
+        }
+
+        System.err.println("count is : " + count);
+    }
+
+}
+
+class DFSTree{
+    public void dfs(int[][] arr,boolean[] visited,int start){
+        visited[start] = true;
+        for(int i=0;i<arr.length;i++){
+            if(visited[i] == false && arr[start][i] == 1){
+                dfs(arr, visited, i);
+            }
+        }
+    }
+}
+
